4d1fe367bbbd284a685282fd560d1a5b20b1e50d
Anteste <reversodr@gmail.com>
2020-06-23 16:43:32 +0100

    Logout Feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,14 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class AccountController extends AbstractController
 {
     /**
+     * Permet d'afficher et de gérer le formulaire de connexion
+     *
      * @Route("/login", name="account_login")
+     *
+     * @return Response
      */
     public function login()
     {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Permet de se déconnecter
+     *
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout()
+    {
+
     }
 }
